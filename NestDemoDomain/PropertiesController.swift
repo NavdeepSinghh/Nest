e67f6528eb9fc8e10111119e8c5ba6d7af611bb9
Navdeep  Singh <navdeep.singh@sonartechnologies.com>
2017-05-22 14:59:41 +1000

    Dummy Collectionview Cells
    CollectionViewDelegate methods + Dummy cells

--- NestDemoDomain/PropertiesController.swift
+++ NestDemoDomain/PropertiesController.swift
@@ -8,17 +8,29 @@
 
 import UIKit
 
+let cellId = "propertyCell"
+
 class PropertiesController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Properties"
         collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        collectionView?.alwaysBounceVertical = true
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    
+    // Configuring collectionView
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = UIColor.red
+        return cell
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
 
